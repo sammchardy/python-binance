9ed981430727a318da63f496a4d8f4ea0eff4749
Sam McHardy <hello@sammchardy.net>
2017-08-25 15:23:20 +0930

    Remove 2.6

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.2',
