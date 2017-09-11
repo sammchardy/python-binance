5d5adf8f4d1c1cfbbe966d3bcb9d7bba84643e4e
Sam McHardy <hello@sammchardy.net>
2017-09-12 09:06:20 +0930

    Update to v0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-binance',
-    version='0.1.4',
+    version='0.1.5',
     packages=['binance'],
     description='Binance API python implementation',
     url='https://github.com/sammchardy/python-binance',
