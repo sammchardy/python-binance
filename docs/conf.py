748748edda46d14be3587ebb98050802d060b0e2
Sam McHardy <hello@sammchardy.net>
2017-10-27 20:08:36 +0930

    Use RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'agogo'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
