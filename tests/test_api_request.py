9581334db472c8ad8dbff0766ec74ed6dfa20d6f
Sam McHardy <hello@sammchardy.net>
2017-10-30 19:47:47 +0930

    Add test for withdraw exception response

--- tests/test_api_request.py
+++ tests/test_api_request.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 from binance.client import Client
-from binance.exceptions import BinanceAPIException, BinanceRequestException
+from binance.exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
 import pytest
 import requests_mock
 
@@ -27,3 +27,14 @@ def test_api_exception():
             json_obj = {"code": 1002, "msg": "Invalid API call"}
             m.get('https://www.binance.com/api/v1/time', json=json_obj, status_code=400)
             client.get_server_time()
+
+
+def test_withdraw_api_exception():
+    """Test Withdraw API response Exception"""
+
+    with pytest.raises(BinanceWithdrawException):
+
+        with requests_mock.mock() as m:
+            json_obj = {"success": False, "msg": "Insufficient funds"}
+            m.register_uri('POST', requests_mock.ANY, json=json_obj, status_code=200)
+            client.withdraw(asset='BTC', address='BTCADDRESS', amount=100)
