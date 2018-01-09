066f769eb13134136b9776a4f9021dd13110c3f0
Sam McHardy <hello@sammchardy.net>
2018-01-09 16:46:32 +1100

    Add function to fetch historical klines

--- binance/client.py
+++ binance/client.py
@@ -6,6 +6,7 @@ import hmac
 import requests
 import time
 from operator import itemgetter
+from .helpers import date_to_milliseconds, interval_to_milliseconds
 from .exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
 
 
@@ -590,6 +591,81 @@ class Client(object):
 
         """
         return self._get('klines', data=params)
+
+    def get_historical_klines(self, symbol, interval, start_str, end_str=None):
+        """Get Historical Klines from Binance
+
+        See dateparse docs for valid start and end string formats http://dateparser.readthedocs.io/en/latest/
+
+        If using offset strings for dates add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
+
+        :param symbol: Name of symbol pair e.g BNBBTC
+        :type symbol: str
+        :param interval: Biannce Kline interval
+        :type interval: str
+        :param start_str: Start date string in UTC format
+        :type start_str: str
+        :param end_str: optional - end date string in UTC format
+        :type end_str: str
+
+        :return: list of OHLCV values
+
+        """
+        # init our list
+        output_data = []
+
+        # setup the max limit
+        limit = 500
+
+        # convert interval to useful value in seconds
+        timeframe = interval_to_milliseconds(interval)
+
+        # convert our date strings to milliseconds
+        start_ts = date_to_milliseconds(start_str)
+
+        # if an end time was passed convert it
+        end_ts = None
+        if end_str:
+            end_ts = date_to_milliseconds(end_str)
+
+        idx = 0
+        # it can be difficult to know when a symbol was listed on Binance so allow start time to be before list date
+        symbol_existed = False
+        while True:
+            # fetch the klines from start_ts up to max 500 entries or the end_ts if set
+            temp_data = self.get_klines(
+                symbol=symbol,
+                interval=interval,
+                limit=limit,
+                startTime=start_ts,
+                endTime=end_ts
+            )
+
+            # handle the case where our start date is before the symbol pair listed on Binance
+            if not symbol_existed and len(temp_data):
+                symbol_existed = True
+
+            if symbol_existed:
+                # append this loops data to our output data
+                output_data += temp_data
+
+                # update our start timestamp using the last value in the array and add the interval timeframe
+                start_ts = temp_data[len(temp_data) - 1][0] + timeframe
+            else:
+                # it wasn't listed yet, increment our start date
+                start_ts += timeframe
+
+            idx += 1
+            # check if we received less than the required limit and exit the loop
+            if len(temp_data) < limit:
+                # exit the while loop
+                break
+
+            # sleep after every 3rd call to be kind to the API
+            if idx % 3 == 0:
+                time.sleep(1)
+
+        return output_data
 
     def get_ticker(self, **params):
         """24 hour price change statistics.
