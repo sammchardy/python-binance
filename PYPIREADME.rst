745ca91200bbc520729c3fc3c095a30f0724a341
Sam McHardy <hello@sammchardy.net>
2018-01-12 21:31:37 +1100

    Update to v0.6.2

--- PYPIREADME.rst
+++ PYPIREADME.rst
@@ -1,5 +1,5 @@
 ================================
-Welcome to python-binance v0.6.1
+Welcome to python-binance v0.6.2
 ================================
 
 .. image:: https://img.shields.io/pypi/v/python-binance.svg
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-binance',
-    version='0.6.1',
+    version='0.6.2',
     packages=['binance'],
     description='Binance REST API python implementation',
     url='https://github.com/sammchardy/python-binance',
