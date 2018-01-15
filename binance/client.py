c1fe80abbb43c5855a2feabf75b257a88b368bc8
Eric Toombs <etoombs@adga.ca>
2018-01-14 22:44:29 -0500

    OK, this time I think I got it all.

--- binance/client.py
+++ binance/client.py
@@ -5,6 +5,7 @@ import time
 from operator import itemgetter
 from .helpers import date_to_milliseconds, interval_to_milliseconds
 from .exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
+from constants import *
 
 
 class Client:
