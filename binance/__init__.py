4da4983c8c598c7bf4bf021118d1bb841b92f54a
Sam McHardy <hello@sammchardy.net>
2018-08-08 19:50:42 +1000

    Include handy imports

--- binance/__init__.py
+++ binance/__init__.py
@@ -5,3 +5,8 @@
 """
 
 __version__ = '0.6.8'
+
+from binance.client import Client
+from binance.client_async import AsyncClient
+from binance.depthcache import DepthCacheManager
+from binance.websockets import BinanceSocketManager
