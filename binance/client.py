6a70992ffaab53ea55fed53b4eae264b338f0c2c
Sam McHardy <hello@sammchardy.net>
2019-01-23 21:11:06 +1100

    Fix static method

--- binance/client.py
+++ binance/client.py
@@ -202,7 +202,7 @@ class Client(BaseClient):
         return self._handle_response(response)
 
     @staticmethod
-    def _handle_response(self, response):
+    def _handle_response(response):
         """Internal helper for handling API responses from the Binance server.
         Raises the appropriate exceptions when necessary; otherwise, returns the
         response.
