176548d1423556c684c3e74964e60ba6a0c1f2bd
Sam McHardy <hello@sammchardy.net>
2019-05-09 21:10:52 +1000

    Remove py3.4

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python',
