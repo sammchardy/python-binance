6c6469770df8631b1b532feb8d726b177d8aa171
vikramaditya <vikramaditya91@gmail.com>
2020-06-27 22:33:04 +0200

    Added missing requirement of websockets

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author_email='',
     install_requires=[
         'requests', 'six', 'pyOpenSSL', 'service-identity', 'dateparser', 'urllib3', 'chardet', 'certifi',
-        'cryptography', 'aiohttp'
+        'cryptography', 'aiohttp', 'websockets'
     ],
     keywords='binance exchange rest api bitcoin ethereum btc eth neo',
     classifiers=[
