66a9c4680450e095bc5b44464429a91cdd2834ba
Alper Alimoglu <alper.alimoglu@gmail.com>
2021-02-11 18:42:18 +0300

    Update client.py
    Small update

--- binance/client.py
+++ binance/client.py
@@ -3038,6 +3038,36 @@ class Client(object):
         params['type'] = 1
         return self._request_margin_api('post', 'futures/transfer', signed=True, data=params)
 
+    def transfer_spot_to_futures(self, **params):
+        """Execute transfer between spot account and futures account.
+
+        https://binance-docs.github.io/apidocs/futures/en/#new-future-account-transfer
+
+        :param asset: name of the asset
+        :type asset: str
+        :param amount: amount to transfer
+        :type amount: str
+        :param recvWindow: the number of milliseconds the request is valid for
+        :type recvWindow: int
+
+        .. code:: python
+
+            transfer = client.transfer_futures_to_spot(asset='USDT', amount='1.1')
+
+        :returns: API response
+
+        .. code-block:: python
+
+            {
+                "tranId": 100000001
+            }
+
+        :raises: BinanceRequestException, BinanceAPIException
+
+        """
+        params['type'] = 1
+        return self._request_margin_api('post', 'futures/transfer', signed=True, data=params)
+
     def transfer_futures_to_spot(self, **params):
         """Execute transfer between spot account and futures account.
 
@@ -3052,7 +3082,7 @@ class Client(object):
 
         .. code:: python
 
-            transfer = client.transfer_spot_to_margin(asset='USDT', amount='1.1')
+            transfer = client.transfer_futures_to_spot(asset='USDT', amount='1.1')
 
         :returns: API response
 
