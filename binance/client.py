c83e3a1fd02b9228ab7699030cad6fe939f20660
AlexNik <zvava@ya.ru>
2021-03-18 01:14:03 +0300

    Docs fix.

--- binance/client.py
+++ binance/client.py
@@ -988,7 +988,7 @@ class Client(object):
                 "mins": 5,
                 "price": "9.35751834"
             }
-"""
+        """
         return self._get('avgPrice', data=params, version=self.PRIVATE_API_VERSION)
 
     def get_ticker(self, **params):
@@ -5599,12 +5599,11 @@ class Client(object):
         :type limit: int
         :param recvWindow: optional
         :type recvWindow: int
-        :param timestamp: requred
-        :type timestamp: int
 
         :returns: API response
 
         .. code-block:: python
+
             {
                "code":200, // 200 for success; others are error codes
                "msg":"", // error message
@@ -5630,7 +5629,11 @@ class Client(object):
                   }
                ]
             }
+
         OR
+
+        .. code-block:: python
+
             {
                "code":200, // 200 for success; others are error codes
                "msg":"", // error message
@@ -5657,7 +5660,11 @@ class Client(object):
                   }
                ]
             }
+
         OR
+
+        .. code-block:: python
+
             {
                "code":200, // 200 for success; others are error codes
                "msg":"", // error message
