49cc3fd15342fa0e97d0580b3d967c77eb84c191
cjna <cjna@umich.edu>
2021-03-28 21:38:46 -1000

    taking an entry out from id

--- binance/client.py
+++ binance/client.py
@@ -2430,7 +2430,7 @@ class Client(object):
             if 'id' in entry and entry['id'] == withdraw_id:
                 return entry
         
-        raise Exception("There is no entry with withdraw id", result)
+        raise Exception("There is no entry with withdraw id")
 
     def get_deposit_address(self, **params):
         """Fetch a deposit address for a symbol
