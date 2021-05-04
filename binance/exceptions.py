28e354c5641bbfe6731140c863988dcd1e0d49a7
Sam McHardy <hello@sammchardy.net>
2021-05-04 21:49:45 +1000

    clean up super calls

--- binance/exceptions.py
+++ binance/exceptions.py
@@ -43,35 +43,35 @@ class BinanceOrderMinAmountException(BinanceOrderException):
 
     def __init__(self, value):
         message = "Amount must be a multiple of %s" % value
-        super(BinanceOrderMinAmountException, self).__init__(-1013, message)
+        super().__init__(-1013, message)
 
 
 class BinanceOrderMinPriceException(BinanceOrderException):
 
     def __init__(self, value):
         message = "Price must be at least %s" % value
-        super(BinanceOrderMinPriceException, self).__init__(-1013, message)
+        super().__init__(-1013, message)
 
 
 class BinanceOrderMinTotalException(BinanceOrderException):
 
     def __init__(self, value):
         message = "Total must be at least %s" % value
-        super(BinanceOrderMinTotalException, self).__init__(-1013, message)
+        super().__init__(-1013, message)
 
 
 class BinanceOrderUnknownSymbolException(BinanceOrderException):
 
     def __init__(self, value):
         message = "Unknown symbol %s" % value
-        super(BinanceOrderUnknownSymbolException, self).__init__(-1013, message)
+        super().__init__(-1013, message)
 
 
 class BinanceOrderInactiveSymbolException(BinanceOrderException):
 
     def __init__(self, value):
         message = "Attempting to trade an inactive symbol %s" % value
-        super(BinanceOrderInactiveSymbolException, self).__init__(-1013, message)
+        super().__init__(-1013, message)
 
 
 class BinanceWebsocketUnableToConnect(Exception):
