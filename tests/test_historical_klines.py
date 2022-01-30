86c2dd195812f453c606057aa33f2263902f9725
MJ Rossetti <s2t2@users.noreply.github.com>
2022-01-29 20:46:09 -0500

    Integration test

--- tests/test_historical_klines.py
+++ tests/test_historical_klines.py
@@ -2,52 +2,58 @@
 # coding=utf-8
 
 from binance.client import Client
+from binance.enums import OHLC_COLUMNS
+from binance.decorators import HistoricalKlinesData
+
+from pandas import DataFrame
+
 import pytest
 import requests_mock
 
+
 client = Client("api_key", "api_secret")
+
+first_available_res = [
+    [
+        1500004800000,
+        "0.00005000",
+        "0.00005300",
+        "0.00001000",
+        "0.00004790",
+        "663152.00000000",
+        1500004859999,
+        "30.55108144",
+        43,
+        "559224.00000000",
+        "25.65468144",
+        "83431971.04346950",
+    ]
+]
+
+first_res = []
+row = [
+    1519892340000,
+    "0.00099400",
+    "0.00099810",
+    "0.00099400",
+    "0.00099810",
+    "4806.04000000",
+    1519892399999,
+    "4.78553253",
+    154,
+    "1785.14000000",
+    "1.77837524",
+    "0",
+]
+
+for i in range(0, 500):
+    first_res.append(row)
+
+second_res = []
 
 
 def test_exact_amount():
     """Test Exact amount returned"""
-
-    first_available_res = [
-        [
-            1500004800000,
-            "0.00005000",
-            "0.00005300",
-            "0.00001000",
-            "0.00004790",
-            "663152.00000000",
-            1500004859999,
-            "30.55108144",
-            43,
-            "559224.00000000",
-            "25.65468144",
-            "83431971.04346950",
-        ]
-    ]
-
-    first_res = []
-    row = [
-        1519892340000,
-        "0.00099400",
-        "0.00099810",
-        "0.00099400",
-        "0.00099810",
-        "4806.04000000",
-        1519892399999,
-        "4.78553253",
-        154,
-        "1785.14000000",
-        "1.77837524",
-        "0",
-    ]
-
-    for i in range(0, 500):
-        first_res.append(row)
-
-    second_res = []
 
     with requests_mock.mock() as m:
         m.get(
@@ -281,4 +287,55 @@ def test_historical_kline_generator_empty_response():
             next(klines)
 
 
-#def test_ohlc_decorators():
+
+
+
+
+def test_test_historical_kline_output_format():
+    """Test output format"""
+
+    with requests_mock.mock() as m:
+        m.get(
+            "https://api.binance.com/api/v3/klines?interval=1m&limit=1&startTime=0&symbol=BNBBTC",
+            json=first_available_res,
+        )
+        m.get(
+            "https://api.binance.com/api/v3/klines?interval=1m&limit=500&startTime=1519862400000&symbol=BNBBTC",
+            json=first_res,
+        )
+        m.get(
+            "https://api.binance.com/api/v3/klines?interval=1m&limit=500&startTime=1519892400000&symbol=BNBBTC",
+            json=second_res,
+        )
+
+        #
+        # DEFAULT FORMAT
+        #
+        klines_list = client.get_historical_klines(
+            symbol="BNBBTC", interval=Client.KLINE_INTERVAL_1MINUTE, start_str="1st March 2018"
+        )
+        assert len(klines_list) == 500
+        assert isinstance(klines_list, list)
+        assert isinstance(klines_list[0], list)
+
+        #
+        # DATAFRAME FORMAT
+        #
+        klines_df = client.get_historical_klines(
+            symbol="BNBBTC", interval=Client.KLINE_INTERVAL_1MINUTE, start_str="1st March 2018",
+            output_format="df"
+        )
+        assert len(klines_df) == 500
+        assert isinstance(klines_df, DataFrame)
+        assert klines_df.columns.tolist() == OHLC_COLUMNS
+
+        #
+        # DECORATED FORMAT
+        #
+        klines_data = client.get_historical_klines(
+            symbol="BNBBTC", interval=Client.KLINE_INTERVAL_1MINUTE, start_str="1st March 2018",
+            output_format="decorated"
+        )
+        assert len(klines_data) == 500
+        assert isinstance(klines_data, list)
+        assert isinstance(klines_data[0], HistoricalKlinesData)
