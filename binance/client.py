0683f6c3d6cf8d510760d7e0d73311790bfed00b
zig <zig@zerodivision.tech>
2023-10-19 14:44:09 +0800

    support eth staking

--- binance/client.py
+++ binance/client.py
@@ -5472,6 +5472,46 @@ class Client(BaseClient):
         """
         return self._request_margin_api('get', 'staking/personalLeftQuota', signed=True, data=params)
 
+    def subscribe_eth_staking(self, **params):
+        """Subscribe ETH staking
+
+        https://binance-docs.github.io/apidocs/spot/en/#subscribe-eth-staking-trade
+
+        """
+        return self._request_margin_api('post', 'eth-staking/eth/stake', signed=True, data=params)
+
+    def redeem_eth(self, **params):
+        """Redeem ETH
+
+        https://binance-docs.github.io/apidocs/spot/en/#redeem-eth-trade
+
+        """
+        return self._request_margin_api('post', 'eth-staking/eth/redeem', signed=True, data=params)
+
+    def get_eth_rewards_distribution_history(self, **params):
+        """Redeem ETH
+
+        https://binance-docs.github.io/apidocs/spot/en/#get-eth-rewards-distribution-history-user_data
+
+        """
+        return self._request_margin_api('get', 'eth-staking/eth/history/rewardsHistory', signed=True, data=params)
+
+    def wrap_beth(self, **params):
+        """Wrap BETH
+
+        https://binance-docs.github.io/apidocs/spot/en/#wrap-beth-trade
+
+        """
+        return self._request_margin_api('post', 'eth-staking/wbeth/wrap', signed=True, data=params)
+
+    def unwrap_beth(self, **params):
+        """Unwrap BETH
+
+        https://binance-docs.github.io/apidocs/spot/en/#unwrap-wbeth-trade
+
+        """
+        return self._request_margin_api('post', 'eth-staking/wbeth/unwrap', signed=True, data=params)
+
     # US Staking Endpoints
 
     def get_staking_asset_us(self, **params):
