b46afffd72fb35615acebff2b81f31c328f9591d
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-10-20 16:00:08 +0100

    add more tests

--- tests/test_ids.py
+++ tests/test_ids.py
@@ -22,6 +22,20 @@ def test_spot_id():
         assert url_dict['newClientOrderId'].startswith('x-R4BD3S82')
 
 
+def test_spot_limit_id():
+    with requests_mock.mock() as m:
+        m.post("https://api.binance.com/api/v3/order", json={}, status_code=200)
+        client.order_limit_buy(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
+        url_dict =  dict(pair.split('=') for pair in m.last_request.text.split('&'))
+        assert url_dict['newClientOrderId'].startswith('x-R4BD3S82')
+
+def test_spot_market_id():
+    with requests_mock.mock() as m:
+        m.post("https://api.binance.com/api/v3/order", json={}, status_code=200)
+        client.order_market_buy(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
+        url_dict =  dict(pair.split('=') for pair in m.last_request.text.split('&'))
+        assert url_dict['newClientOrderId'].startswith('x-R4BD3S82')
+
 def test_swap_id():
     with requests_mock.mock() as m:
         m.post("https://fapi.binance.com/fapi/v1/order", json={}, status_code=200)
