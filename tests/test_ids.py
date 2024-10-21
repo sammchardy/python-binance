983270678501ac9d76e1c500c25220379716dcf5
Carlo Revelli <carlo.revelli@berkeley.edu>
2024-10-21 12:56:01 -0700

    add x- to tests

--- tests/test_ids.py
+++ tests/test_ids.py
@@ -15,21 +15,21 @@ def test_spot_id():
         assert url_dict['side'] == 'BUY'
         assert url_dict['type'] == 'MARKET'
         assert url_dict['quantity'] == '0.1'
-        assert url_dict['newClientOrderId'].startswith('HNA2TXFJ')
+        assert url_dict['newClientOrderId'].startswith('x-HNA2TXFJ')
 
 def test_spot_limit_id():
     with requests_mock.mock() as m:
         m.post("https://api.binance.com/api/v3/order", json={}, status_code=200)
         client.order_limit_buy(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         url_dict =  dict(pair.split('=') for pair in m.last_request.text.split('&'))
-        assert url_dict['newClientOrderId'].startswith('HNA2TXFJ')
+        assert url_dict['newClientOrderId'].startswith('x-HNA2TXFJ')
 
 def test_spot_market_id():
     with requests_mock.mock() as m:
         m.post("https://api.binance.com/api/v3/order", json={}, status_code=200)
         client.order_market_buy(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         url_dict =  dict(pair.split('=') for pair in m.last_request.text.split('&'))
-        assert url_dict['newClientOrderId'].startswith('HNA2TXFJ')
+        assert url_dict['newClientOrderId'].startswith('x-HNA2TXFJ')
 
 def test_swap_id():
     with requests_mock.mock() as m:
@@ -41,7 +41,7 @@ def test_swap_id():
         assert url_dict['side'] == 'buy'
         assert url_dict['type'] == 'market'
         assert url_dict['quantity'] == '0.1'
-        assert url_dict['newClientOrderId'.lower()].startswith('Cb7ytekJ'.lower())
+        assert url_dict['newClientOrderId'.lower()].startswith('x-Cb7ytekJ'.lower())
 
 
 def test_papi_um_id():
@@ -54,7 +54,7 @@ def test_papi_um_id():
         assert url_dict['side'] == 'BUY'
         assert url_dict['type'] == 'MARKET'
         assert url_dict['quantity'] == '0.1'
-        assert url_dict['newClientOrderId'].startswith('Cb7ytekJ')
+        assert url_dict['newClientOrderId'].startswith('x-Cb7ytekJ')
 
 
 def test_papi_cm_id():
@@ -67,7 +67,7 @@ def test_papi_cm_id():
         assert url_dict['side'] == 'BUY'
         assert url_dict['type'] == 'MARKET'
         assert url_dict['quantity'] == '0.1'
-        assert url_dict['newClientOrderId'].startswith('Cb7ytekJ')
+        assert url_dict['newClientOrderId'].startswith('x-Cb7ytekJ')
 
 
 @pytest.mark.asyncio()
@@ -76,7 +76,7 @@ async def test_spot_id_async():
     with aioresponses() as m:
         def handler(url, **kwargs):
             client_order_id = kwargs['data'][0][1]
-            assert client_order_id.startswith('HNA2TXFJ')
+            assert client_order_id.startswith('x-HNA2TXFJ')
         m.post("https://api.binance.com/api/v3/order", payload={'id': 1}, status=200, callback=handler)
         await clientAsync.create_order(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         await clientAsync.close_connection()
@@ -88,7 +88,7 @@ async def test_swap_id_async():
     with aioresponses() as m:
         def handler(url, **kwargs):
             client_order_id = kwargs['data'][0][1]
-            assert client_order_id.startswith('Cb7ytekJ')
+            assert client_order_id.startswith('x-Cb7ytekJ')
         m.post("https://fapi.binance.com/fapi/v1/order", payload={'id': 1}, status=200, callback=handler)
         await clientAsync.futures_create_order(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         await clientAsync.close_connection()
@@ -99,7 +99,7 @@ async def test_papi_um_id_async():
     with aioresponses() as m:
         def handler(url, **kwargs):
             client_order_id = kwargs['data'][0][1]
-            assert client_order_id.startswith('Cb7ytekJ')
+            assert client_order_id.startswith('x-Cb7ytekJ')
         m.post("https://papi.binance.com/papi/v1/um/order", payload={'id': 1}, status=200, callback=handler)
         await clientAsync.papi_create_um_order(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         await clientAsync.close_connection()
@@ -111,7 +111,7 @@ async def test_papi_cm_id_async():
     with aioresponses() as m:
         def handler(url, **kwargs):
             client_order_id = kwargs['data'][0][1]
-            assert client_order_id.startswith('Cb7ytekJ')
+            assert client_order_id.startswith('x-Cb7ytekJ')
         m.post("https://papi.binance.com/papi/v1/cm/order", payload={'id': 1}, status=200, callback=handler)
         await clientAsync.papi_create_cm_order(symbol="LTCUSDT", side="BUY", type="MARKET", quantity=0.1)
         await clientAsync.close_connection()
