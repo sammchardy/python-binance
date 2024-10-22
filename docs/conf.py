28cedd1210a9e6564fa471182eb4b66c50eeb300
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-10-22 17:56:22 +0100

    fix docs: add requirements

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
