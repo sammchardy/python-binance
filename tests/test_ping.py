cda1d4f2aebf4695b0498c6b1c18b8d4d29a04c4
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-10-23 18:28:12 +0100

    add tests

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -13,13 +13,42 @@ else:
 
 client = Client("api_key", "api_secret", {'proxies': proxies})
 
+def test_papi_ping_sync():
+    ping_response = client.papi_ping()
+    assert ping_response != None
 
 def test_ping_sync():
-    ping_response = client.papi_ping()
+    ping_response = client.ping()
+    assert ping_response != None
+
+def test_futures_ping():
+    ping_response = client.futures_ping()
+    assert ping_response != None
+
+def test_coin_ping():
+    ping_response = client.futures_coin_ping()
+    assert ping_response != None
+
+@pytest.mark.asyncio()
+async def test_papi_ping_async():
+    clientAsync = AsyncClient(api_key="api_key", api_secret="api_secret", https_proxy=proxy)
+    ping_response = await clientAsync.papi_ping()
     assert ping_response != None
 
 @pytest.mark.asyncio()
 async def test_ping_async():
     clientAsync = AsyncClient(api_key="api_key", api_secret="api_secret", https_proxy=proxy)
-    ping_response = await clientAsync.papi_ping()
+    ping_response = await clientAsync.ping()
+    assert ping_response != None
+
+@pytest.mark.asyncio()
+async def test_futures_ping_async():
+    clientAsync = AsyncClient(api_key="api_key", api_secret="api_secret", https_proxy=proxy)
+    ping_response = await clientAsync.futures_ping()
+    assert ping_response != None
+
+@pytest.mark.asyncio()
+async def test_coin_ping_async():
+    clientAsync = AsyncClient(api_key="api_key", api_secret="api_secret", https_proxy=proxy)
+    ping_response = await clientAsync.futures_coin_ping()
     assert ping_response != None
