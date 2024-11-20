d82a8b6694cd514a19e4a887d41a45334702b1bc
pcriadoperez <pablo@criadoperez.com>
2024-11-19 21:10:18 -0500

    merge master

--- binance/async_client.py
+++ binance/async_client.py
@@ -3168,6 +3168,11 @@ class AsyncClient(BaseClient):
         return await self._request_papi_api(
             "post", "margin/repay-debt", signed=True, data=params
         )
+        
+    async def create_oco_order(self, **params):
+        if "listClientOrderId" not in params:
+            params["listClientOrderId"] = self.SPOT_ORDER_PREFIX + self.uuid22()
+        return await self._post("orderList/oco", True, data=params)
 
     ############################################################
     # WebSocket API methods
