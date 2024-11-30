c1db54ef4f87219845a0157ae0c9f13ebbcc570e
Pablo <pablo@criadoperez.com>
2024-11-30 03:37:44 +0100

    feat: add copy button in docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.imgmath",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
