d37715b860bd2fbdffbb69a995cd1408e22238db
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-09-24 16:23:35 +0100

    rm proxy none

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,6 @@ proxies = {}
 proxy = os.getenv("PROXY")
 
 proxy = "http://188.245.226.105:8911"
-proxy = None
 if proxy:
     proxies = {"http": proxy, "https": proxy}  # tmp: improve this in the future
 else:
