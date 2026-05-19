204caf5b1971798d7245cc8c4f80b84e428b47ed
taras <nomail@nomail>
2026-05-19 04:38:02 +0200

    Check for picows version

--- binance/ws/websockets_compat.py
+++ binance/ws/websockets_compat.py
@@ -1,7 +1,31 @@
+import re
+
+
+def _check_picows_version():
+    import picows
+
+    match = re.match(r"^(\d+)(?:\.(\d+))?(?:\.(\d+))?", picows.__version__)
+    if not match:
+        raise ImportError("picows>=2.1.0 is required")
+
+    version = tuple(int(part or 0) for part in match.groups())
+
+    MIN_PICOWS_VERSION = (2, 1, 0)
+    if not version >= MIN_PICOWS_VERSION:
+        raise ImportError("picows>=2.1.0 is required")
+
+
 try:
+    _check_picows_version()
+
     import picows.websockets as websockets
     from picows.websockets import (
-        ConnectionClosed, ConnectionClosedError, ConnectionClosedOK, State, WebSocketClientProtocol, protocol as ws_protocol
+        ConnectionClosed,
+        ConnectionClosedError,
+        ConnectionClosedOK,
+        State,
+        WebSocketClientProtocol,
+        protocol as ws_protocol,
     )
     websockets_package_name = "picows.websockets"
 except ImportError:
